7fa6c2be383ef9ccc822e5aaea73b34369dc9963
lateralusX <lateralusx.github@gmail.com>
2021-09-28 12:53:15 +0200

    Add custom startup-tracer nettrace analysis tool.

---
+++ src/startup-tracer/MonoProfilerTraceEventParser.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Text;
+using Microsoft.Diagnostics.Tracing;
+
+namespace StartupTracer
+{
+    public sealed class MonoProfilerTraceEventParser : TraceEventParser
+    {
+        public static readonly string ProviderName = "Microsoft-DotNETRuntimeMonoProfiler";
+        public static readonly Guid ProviderGuid = new Guid(unchecked((int)0x7F442D82), unchecked((short)0x0F1D), unchecked((short)0x5155), 0x4B, 0x8C, 0x15, 0x29, 0xEB, 0x2E, 0x31, 0xC2);
+
+        public enum Keywords : long
+        {
+            Jit = 0x10,
+        };
+
+        public MonoProfilerTraceEventParser(TraceEventSource source) : base(source) { }
+
+        public event Action<JitTraceData> MonoProfilerJitBegin {
+            add
+            {
+                // action, eventid, taskid, taskName, taskGuid, opcode, opcodeName, providerGuid, providerName
+                source.RegisterEventTemplate(new JitTraceData(value, 8, 1, "MonoProfiler", MonoProfilerTaskGuid, 25, "JitBegin", ProviderGuid, ProviderName));
+            }
+            remove
+            {
+                source.UnregisterEventTemplate(value, 8, ProviderGuid);
+                source.UnregisterEventTemplate(value, 25, MonoProfilerTaskGuid);
+            }
+        }
+
+        public event Action<JitTraceData> MonoProfilerJitDone {
+            add
+            {
+                // action, eventid, taskid, taskName, taskGuid, opcode, opcodeName, providerGuid, providerName
+                source.RegisterEventTemplate(new JitTraceData(value, 10, 1, "MonoProfiler", MonoProfilerTaskGuid, 27, "JitDone", ProviderGuid, ProviderName));
+            }
+            remove
+            {
+                source.UnregisterEventTemplate(value, 10, ProviderGuid);
+                source.UnregisterEventTemplate(value, 27, MonoProfilerTaskGuid);
+            }
+        }
+
+        public event Action<JitTraceDataVerbose> MonoProfilerJitDoneVerbose {
+            add
+            {
+                // action, eventid, taskid, taskName, taskGuid, opcode, opcodeName, providerGuid, providerName
+                source.RegisterEventTemplate(new JitTraceDataVerbose(value, 62, 1, "MonoProfiler", MonoProfilerTaskGuid, 79, "JitDoneVerbose", ProviderGuid, ProviderName));
+            }
+            remove
+            {
+                source.UnregisterEventTemplate(value, 62, ProviderGuid);
+                source.UnregisterEventTemplate(value, 79, MonoProfilerTaskGuid);
+            }
+        }
+
+        private const TraceEventID JitBeginEventID = (TraceEventID)8;
+        private const TraceEventID JitDoneEventID = (TraceEventID)10;
+        private const TraceEventID JitDoneVerboseEventID = (TraceEventID)62;
+
+        protected override string GetProviderName() { return ProviderName; }
+
+        public static Guid GetProviderGuid() { return ProviderGuid; }
+
+        public static ulong GetKeywords() { return (ulong)Keywords.Jit; }
+
+        static private volatile TraceEvent[] s_templates;
+
+        protected override void EnumerateTemplates(Func<string, string, EventFilterResponse> eventsToObserve, Action<TraceEvent> callback)
+        {
+            if (s_templates == null)
+            {
+                var templates = new TraceEvent[3];
+                templates[0] = new JitTraceData(null, 8, 1, "MonoProfiler", MonoProfilerTaskGuid, 25, "JitBegin", ProviderGuid, ProviderName);
+                templates[1] = new JitTraceData(null, 10, 1, "MonoProfiler", MonoProfilerTaskGuid, 27, "JitDone", ProviderGuid, ProviderName);
+                templates[2] = new JitTraceDataVerbose(null, 62, 1, "MonoProfiler", MonoProfilerTaskGuid, 79, "JitDoneVerbose", ProviderGuid, ProviderName);
+                s_templates = templates;
+            }
+            foreach (var template in s_templates)
+                if (eventsToObserve == null || eventsToObserve(template.ProviderName, template.EventName) == EventFilterResponse.AcceptEvent)
+                    callback(template);
+        }
+
+        private static readonly Guid MonoProfilerTaskGuid = new Guid(unchecked((int)0x7EC39CC6), unchecked((short)0xC9E3), unchecked((short)0x4328), 0x9B, 0x32, 0xCA, 0x6C, 0x5E, 0xC0, 0xEF, 0x31);
+    }
+
+    public sealed class JitTraceData : TraceEvent
+    {
+        internal JitTraceData(Action<JitTraceData> action, int eventID, int task, string taskName, Guid taskGuid, int opcode, string opcodeName, Guid providerGuid, string providerName)
+            : base(eventID, task, taskName, taskGuid, opcode, opcodeName, providerGuid, providerName)
+        {
+            Action = action;
+        }
+
+        public long MethodID { get { return GetInt64At(0); } }
+
+        public long ModuleID { get { return GetInt64At(8); } }
+
+        public int MethodToken { get { return GetInt32At(16); } }
+
+        protected override void Dispatch()
+        {
+            Action(this);
+        }
+        protected override Delegate Target {
+            get { return Action; }
+            set { Action = (Action<JitTraceData>)value; }
+        }
+        protected override void Validate()
+        {
+        }
+
+        public override StringBuilder ToXml(StringBuilder sb)
+        {
+            Prefix(sb);
+            XmlAttribHex(sb, "MethodID", MethodID);
+            XmlAttribHex(sb, "ModuleID", ModuleID);
+            XmlAttribHex(sb, "MethodToken", MethodToken);
+            sb.Append("/>");
+            return sb;
+        }
+
+        public override string[] PayloadNames {
+            get
+            {
+                if (payloadNames == null)
+                {
+                    payloadNames = new string[] { "MethodID", "ModuleID", "MethodToken" };
+                }
+
+                return payloadNames;
+            }
+        }
+
+        public override object PayloadValue(int index)
+        {
+            switch (index)
+            {
+                case 0:
+                    return MethodID;
+                case 1:
+                    return ModuleID;
+                case 2:
+                    return MethodToken;
+                default:
+                    return null;
+            }
+        }
+
+        private event Action<JitTraceData> Action;
+    }
+
+    public sealed class JitTraceDataVerbose : TraceEvent
+    {
+        internal JitTraceDataVerbose(Action<JitTraceDataVerbose> action, int eventID, int task, string taskName, Guid taskGuid, int opcode, string opcodeName, Guid providerGuid, string providerName)
+            : base(eventID, task, taskName, taskGuid, opcode, opcodeName, providerGuid, providerName)
+        {
+            Action = action;
+        }
+
+        public long MethodID { get { return GetInt64At(0); } }
+
+        public string MethodNamespace { get { return GetUnicodeStringAt(8); } }
+
+        public string MethodName { get { return GetUnicodeStringAt(SkipUnicodeString(8)); } }
+
+        public string MethodSignature { get { return GetUnicodeStringAt(SkipUnicodeString(SkipUnicodeString(8))); } }
+
+        protected override void Dispatch()
+        {
+            Action(this);
+        }
+        protected override Delegate Target {
+            get { return Action; }
+            set { Action = (Action<JitTraceDataVerbose>)value; }
+        }
+        protected override void Validate()
+        {
+        }
+
+        public override StringBuilder ToXml(StringBuilder sb)
+        {
+            Prefix(sb);
+            XmlAttribHex(sb, "MethodID", MethodID);
+            XmlAttrib(sb, "MethodNamespace", MethodNamespace);
+            XmlAttrib(sb, "MethodName", MethodName);
+            XmlAttrib(sb, "MethodSignature", MethodSignature);
+            sb.Append("/>");
+            return sb;
+        }
+
+        public override string[] PayloadNames {
+            get
+            {
+                if (payloadNames == null)
+                {
+                    payloadNames = new string[] { "MethodID", "MethodNamespace", "MethodName", "MethodSignature" };
+                }
+
+                return payloadNames;
+            }
+        }
+
+        public override object PayloadValue(int index)
+        {
+            switch (index)
+            {
+                case 0:
+                    return MethodID;
+                case 1:
+                    return MethodNamespace;
+                case 2:
+                    return MethodName;
+                case 3:
+                    return MethodSignature;
+                default:
+                    return null;
+            }
+        }
+
+        private event Action<JitTraceDataVerbose> Action;
+    }
+}
---
+++ src/startup-tracer/Program.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Diagnostics.Tracing;
+using Microsoft.Diagnostics.Tracing.Parsers.Clr;
+
+namespace StartupTracer
+{
+    class ExecutionCheckpoint
+    {
+        public ExecutionCheckpoint(string name, DateTime timestamp)
+        {
+            this.Name = name;
+            this.TimeStamp = timestamp;
+        }
+
+        public string Name { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+
+    class MethodData
+    {
+        public MethodData(long methodID, string methodNamespace, string methodName)
+        {
+            this.MethodID = methodID;
+            this.MethodNamespace = methodNamespace;
+            this.MethodName = methodName;
+        }
+
+        public long MethodID { get; set; }
+        public string MethodNamespace { get; set; }
+        public string MethodName { get; set; }
+    }
+
+    class MethodLoad : MethodData
+    {
+        public MethodLoad(long methodID, string methodNamespace, string methodName, DateTime timestamp)
+            : base(methodID, methodNamespace, methodName)
+        {
+            this.TimeStamp = timestamp;
+        }
+
+        public DateTime TimeStamp { get; set; }
+    }
+
+    class Program
+    {
+        static void PrintHelp()
+        {
+            StringBuilder message = new StringBuilder();
+            message.AppendLine("Usage:");
+            message.AppendLine("  startup-tracer [nettrace file] [commands]");
+            message.AppendLine();
+            message.AppendLine(String.Format("{0,-25} {1,0}", "  [nettrace file]", "Path to nettrace file."));
+            message.AppendLine(String.Format("{0,-25} {1,0}", "  --method=[filter]", "Method name(s) to analyze. Each loaded method will be checked against filter."));
+            message.AppendLine(String.Format("{0,-25} {1,0}", "", "Using '*' as method name will analyze all loaded methods."));
+            message.AppendLine(String.Format("{0,-25} {1,0}", "", "Using empty string or leaving out --method command will analyze first loaded method."));
+            message.AppendLine(String.Format("{0,-25} {1,0}", "  --?", "Display help."));
+            message.AppendLine();
+            message.AppendLine("Enable default method load events in dotnet-trace, --providers Microsoft-Windows-DotNETRuntime:10:5");
+            message.AppendLine("Enable MonoProfiler method load events in dotnet-trace, --providers Microsoft-DotNETRuntimeMonoProfiler:10:5:");
+
+            Console.WriteLine(message);
+        }
+
+        static bool IncludeMethod(string methodNamespace, string methodName, string pattern)
+        {
+            var fullMethodName = methodNamespace + "." + methodName;
+            if (pattern == "*" || fullMethodName.Contains(pattern))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        static void ParseRundown(string filePath, string pattern, out List<ExecutionCheckpoint> executionCheckpoints, out Dictionary<long, MethodData> methods)
+        {
+            var rundownExecutionCheckpoints = new List<ExecutionCheckpoint>();
+            var rundownMethods = new Dictionary<long, MethodData>();
+
+            Task streamTask = Task.Run(() =>
+            {
+                long loadedEvents = 0;
+                long exectionCheckPointEvents = 0;
+                long methodEvents = 0;
+
+                Console.Write("Loading rundown events...");
+
+                var source = new EventPipeEventSource(filePath);
+                var rundown = new ClrRundownTraceEventParser(source);
+
+                rundown.ExecutionCheckpointRundownExecutionCheckpointDCEnd += delegate (ExecutionCheckpointTraceData data)
+                {
+                    loadedEvents++;
+                    exectionCheckPointEvents++;
+
+                    if (loadedEvents % 100 == 0)
+                        Console.Write(".");
+
+                    rundownExecutionCheckpoints.Add(new ExecutionCheckpoint(data.CheckpointName, source.QPCTimeToTimeStamp(data.CheckpointTimestamp)));
+                };
+
+                rundown.MethodDCStopVerbose += delegate (MethodLoadUnloadVerboseTraceData data)
+                {
+                    loadedEvents++;
+                    methodEvents++;
+
+                    if (loadedEvents % 100 == 0)
+                        Console.Write(".");
+
+                    if (string.IsNullOrEmpty(pattern) && rundownMethods.Count != 0)
+                        return;
+
+                    if (IncludeMethod(data.MethodNamespace, data.MethodName, pattern))
+                    {
+                        rundownMethods.TryAdd(data.MethodID, new MethodData(data.MethodID, data.MethodNamespace, data.MethodName));
+                    }
+                };
+
+                source.Process();
+
+                Console.WriteLine("Done.");
+                Console.WriteLine($"Parsed {loadedEvents} rundown events, ExecutionCheckpoints ({exectionCheckPointEvents}), Methods ({methodEvents}).");
+            });
+
+            streamTask.Wait();
+
+            executionCheckpoints = rundownExecutionCheckpoints;
+            methods = rundownMethods;
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length < 1)
+                {
+                    PrintHelp();
+                    throw new ArgumentException("Missing trace file argument.");
+                }
+
+                string filePath = args[0];
+                string methodName = "";
+                int maxArgsIndex = args.Length - 1;
+
+                for (int i = 1; i <= maxArgsIndex; i++)
+                {
+                    if (args[i].StartsWith("--method="))
+                    {
+                        methodName = args[i].Substring(9);
+                        continue;
+                    }
+                    else if (args[i].Equals("--?", StringComparison.OrdinalIgnoreCase))
+                    {
+                        PrintHelp();
+                        return;
+                    }
+                    else
+                    {
+                        throw new ArgumentException($"Unknown command, {args[i]}");
+                    }
+                }
+
+                Dictionary<long, MethodData> methods = new Dictionary<long, MethodData>();
+                List<ExecutionCheckpoint> executionCheckpoints = new List<ExecutionCheckpoint>();
+
+                ParseRundown(filePath, methodName, out executionCheckpoints, out methods);
+
+                Dictionary<long, MethodLoad> methodLoads = new Dictionary<long, MethodLoad>();
+                Task streamTask = Task.Run(() =>
+                {
+                    var source = new EventPipeEventSource(filePath);
+                    var monoProfiler = new MonoProfilerTraceEventParser(source);
+
+                    monoProfiler.MonoProfilerJitDone += delegate (JitTraceData data)
+                    {
+                        if (string.IsNullOrEmpty(methodName) && methodLoads.Count != 0)
+                            return;
+
+                        // Needs to load method data from dictionary.
+                        MethodData method;
+                        if (methods.TryGetValue(data.MethodID, out method))
+                        {
+                            if (IncludeMethod(method.MethodNamespace, method.MethodName, methodName))
+                            {
+                                methodLoads.TryAdd(data.MethodID, new MethodLoad(method.MethodID, method.MethodNamespace, method.MethodName, data.TimeStamp));
+                            }
+                        }
+                    };
+
+                    monoProfiler.MonoProfilerJitDoneVerbose += delegate (JitTraceDataVerbose data)
+                    {
+                        if (string.IsNullOrEmpty(methodName) && methodLoads.Count != 0)
+                            return;
+
+                        if (IncludeMethod(data.MethodNamespace, data.MethodName, methodName))
+                        {
+                            methodLoads.TryAdd(data.MethodID, new MethodLoad(data.MethodID, data.MethodNamespace, data.MethodName, data.TimeStamp));
+                        }
+                    };
+
+                    source.Clr.AddCallbackForEvent("Method/Load", (MethodLoadUnloadTraceData data) =>
+                    {
+                        if (string.IsNullOrEmpty(methodName) && methodLoads.Count != 0)
+                            return;
+
+                        // Needs to load method data from dictionary.
+                        MethodData method;
+                        if (methods.TryGetValue(data.MethodID, out method))
+                        {
+                            if (IncludeMethod(method.MethodNamespace, method.MethodName, methodName))
+                            {
+                                methodLoads.TryAdd(data.MethodID, new MethodLoad(method.MethodID, method.MethodNamespace, method.MethodName, data.TimeStamp));
+                            }
+                        }
+
+                    });
+
+                    source.Clr.AddCallbackForEvent("Method/LoadVerbose", (MethodLoadUnloadVerboseTraceData data) =>
+                    {
+                        if (string.IsNullOrEmpty(methodName) && methodLoads.Count != 0)
+                            return;
+
+                        if (IncludeMethod(data.MethodNamespace, data.MethodName, methodName))
+                        {
+                            methodLoads.TryAdd(data.MethodID, new MethodLoad(data.MethodID, data.MethodNamespace, data.MethodName, data.TimeStamp));
+                        }
+                    });
+
+                    source.Process();
+                });
+
+                streamTask.Wait();
+
+                if (methodLoads.Count == 0)
+                    throw new Exception($"Couldn't find method = {methodName} in trace file = {filePath}");
+
+                ExecutionCheckpoint initRuntime = null;
+                ExecutionCheckpoint suspendRuntime = null;
+                ExecutionCheckpoint resumeRuntime = null;
+
+                foreach (var checkpoint in executionCheckpoints)
+                {
+                    if (checkpoint.Name.Contains("RuntimeInit"))
+                    {
+                        initRuntime = checkpoint;
+                        continue;
+                    }
+                    else if (checkpoint.Name.Contains("RuntimeSuspend"))
+                    {
+                        suspendRuntime = checkpoint;
+                        continue;
+                    }
+                    else if (checkpoint.Name.Contains("RuntimeResume"))
+                    {
+                        resumeRuntime = checkpoint;
+                        continue;
+                    }
+                }
+
+                var suspended = resumeRuntime.TimeStamp - suspendRuntime.TimeStamp;
+
+                int maxScreenWidth = Console.WindowWidth;
+                int TableMethodIDWidth = (int)(maxScreenWidth * 0.15);
+                int TableNameWidth = (int)(maxScreenWidth * 0.75);
+                int TableTimeWidth = (int)(maxScreenWidth * 0.1);
+                int MaxNameWidth = TableNameWidth - 10;
+
+                if (MaxNameWidth < 10)
+                    MaxNameWidth = 10;
+
+                var tableFormat = "{0,-" + TableMethodIDWidth + "}{1,-" + TableNameWidth + "}{2,-" + TableTimeWidth + "}";
+
+                Console.WriteLine(Environment.NewLine + $"Time between runtime init and specific method load, measured in milliseconds, method filter = '{methodName}'" + Environment.NewLine);
+
+                Console.WriteLine(String.Format(tableFormat, "MethodID", "Method", "MSecs"));
+                Console.WriteLine(String.Format(tableFormat, "--------", "------", "-----"));
+
+                foreach (var methodLoad in methodLoads)
+                {
+                    var diff = methodLoad.Value.TimeStamp - initRuntime.TimeStamp - suspended;
+                    var name = methodLoad.Value.MethodNamespace + "." + methodLoad.Value.MethodName;
+                    if (name.Length > MaxNameWidth)
+                    {
+                        name = methodLoad.Value.MethodName;
+                        if (name.Length < MaxNameWidth)
+                        {
+                            int left = MaxNameWidth - name.Length;
+                            var nsItems = methodLoad.Value.MethodNamespace.Split('.');
+                            for (int item = nsItems.Length - 1; item >= 0; item--)
+                            {
+                                if (left - nsItems[item].Length < 0)
+                                {
+                                    name = "..." + name;
+                                    break;
+                                }
+                                name = nsItems[item] + "." + name;
+                                left -= nsItems[item].Length;
+                            }
+                        }
+                        else
+                        {
+                            name = "..." + name.Substring (name.Length - MaxNameWidth);
+                        }
+                    }
+                    Console.WriteLine(String.Format(tableFormat, "0x" + methodLoad.Value.MethodID.ToString("X"), name, (int)diff.TotalMilliseconds));
+                }
+            }
+            catch (Exception ex)
+            {
+                var currentColor = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+                Console.ForegroundColor = currentColor;
+            }
+
+            return;
+        }
+    }
+}
